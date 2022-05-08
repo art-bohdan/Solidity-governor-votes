dbb30fb62de6e3e0c17dc4fb6607936462119917
art-bohdan <bohdanprog@gmail.com>
2022-05-08 13:58:16 +0200

    add to erc20 token contract override function call

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IERC20.sol";
 
 contract ERC20 is IERC20 {
   //Variables
   uint256 public _totalSupply;
 
   mapping(address => uint256) public _balanceOf;
-  mapping(address => mapping(address => uint256)) public override allowance;
+  mapping(address => mapping(address => uint256)) public allowance;
 
   string public _name;
   string public _symbol;
@@ -25,18 +25,18 @@ contract ERC20 is IERC20 {
     _mint(totalSupply_);
   }
 
-//Function
-  function name() public view returns (string memory) {
+  //Function
+  function name() public view override returns (string memory) {
     return _name;
   }
 
   // returns Symbol token
-  function symbol() public view returns (string memory) {
+  function symbol() public view override returns (string memory) {
     return _symbol;
   }
 
   // returns decimals
-  function decimals() public view returns (uint8) {
+  function decimals() public view override returns (uint256) {
     return _decimals;
   }
 
@@ -45,85 +45,37 @@ contract ERC20 is IERC20 {
     return _balanceOf[account];
   }
 
-  function increaseAllowance(address spender, uint256 addedValue)
-    public
-    payable
-    returns (bool)
-  {
-    require(
-      spender != address(0),
-      "ERC20: spender should be not a zero address"
-    );
+  function increaseAllowance(address spender, uint256 addedValue) public payable returns (bool) {
+    require(spender != address(0), "ERC20: spender should be not a zero address");
     allowance[msg.sender][spender] += addedValue;
     emit Approval(msg.sender, spender, addedValue);
     return true;
   }
 
-  function decreaseAllowance(address spender, uint256 addedValue)
-    public
-    payable
-    returns (bool)
-  {
-    require(
-      spender != address(0),
-      "ERC20: spender should be not a zero address"
-    );
+  function decreaseAllowance(address spender, uint256 addedValue) public payable returns (bool) {
+    require(spender != address(0), "ERC20: spender should be not a zero address");
     allowance[msg.sender][spender] -= addedValue;
     emit Approval(msg.sender, spender, addedValue);
     return true;
   }
 
-//   function _beforeTokenTransfer(
-//     address operator, 
-//     address from, 
-//     address to, 
-//     uint256 amount
-// ) 
-//     internal 
-//     override 
-//     virtual 
-// {
-//     if (from != Utils.EMPTY_ADDRESS) {
-//         updateAccountHistory(from, balanceOf(from).sub(amount));
-//     }
-//     if (to != Utils.EMPTY_ADDRESS) {
-//         updateAccountHistory(to, balanceOf(to).add(amount));
-//     }
-//     super._beforeTokenTransfer(operator, from, to, amount);
-// }
-
   function totalSupply() public view override returns (uint256) {
     return _totalSupply;
   }
 
-  function _allowance(address owner, address spender)
-    external
-    view
-    returns (uint256)
-  {
+  function _allowance(address owner, address spender) external view returns (uint256) {
     return allowance[owner][spender];
   }
 
-  function transfer(address to, uint256 amount)
-    external
-    override
-    returns (bool)
-  {
-    require(
-      _balanceOf[msg.sender] >= amount,
-      "ERC20: transfer amount exceeds balance"
-    );
+  function transfer(address to, uint256 amount) external override returns (bool) {
+    require(_balanceOf[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
     _balanceOf[msg.sender] -= amount;
     _balanceOf[to] += amount;
     emit Transfer(msg.sender, to, amount);
     return true;
   }
 
-  function approve(address spender, uint256 amount)
-    external
-    override
-    returns (bool)
-  {
+  function approve(address spender, uint256 amount) external override returns (bool) {
     require(spender != address(0), "ERC20: approve to the zero address");
     allowance[msg.sender][spender] = amount;
     emit Approval(msg.sender, spender, amount);
@@ -136,10 +88,7 @@ contract ERC20 is IERC20 {
     uint256 amount
   ) external override returns (bool) {
     require(_balanceOf[from] >= amount, "ERC20: not enough funds on account");
-    require(
-      allowance[from][msg.sender] >= amount,
-      "ERC20: not enough allowance funds on account"
-    );
+    require(allowance[from][msg.sender] >= amount, "ERC20: not enough allowance funds on account");
     allowance[from][msg.sender] -= amount;
     _balanceOf[from] -= amount;
     _balanceOf[to] += amount;
@@ -149,10 +98,7 @@ contract ERC20 is IERC20 {
 
   function burnFrom(address from, uint256 amount) external returns (bool) {
     require(_balanceOf[from] >= amount, "ERC20: not enough funds on account");
-    require(
-      allowance[from][msg.sender] >= amount,
-      "ERC20: not enough allowance funds"
-    );
+    require(allowance[from][msg.sender] >= amount, "ERC20: not enough allowance funds");
     allowance[from][msg.sender] -= amount;
     _balanceOf[from] -= amount;
     _totalSupply -= amount;
