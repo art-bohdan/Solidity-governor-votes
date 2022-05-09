db42a6f39b0c4c7ec2754418aaea67942191305e
itelab <itelab@itelabs-MacBook-Pro.local>
2022-05-09 16:53:56 +0200

    fixed vote function, get vote from user, get voting details

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -5,13 +5,13 @@ import "./IERC20.sol";
 
 contract Governance {
   //Variables
-  mapping(address => UserWhitelist userWhitelist) whitelistAddress;
-  mapping(address => VoteStatus voteStatus) public votings;
+  mapping(address => UserWhitelist) whitelistAddress;
+  mapping(address => VoteStatus) public votings;
   VotingDetails votingDetails;
 
   struct UserWhitelist {
-  bool inWhitelist;
-  bool executed;
+    bool inWhitelist;
+    bool executed;
   }
 
   struct VotingDetails {
@@ -55,13 +55,14 @@ contract Governance {
   }
 
   modifier isWhitelisted() {
-    require(whitelistAddress[msg.sender] == true, "You need to be whitelisted");
+    require(whitelistAddress[msg.sender].inWhitelist == true, "You need to be whitelisted");
     _;
   }
 
   //Events
-  event VotePlaced(uint256 indexed votingId, address voter, VoteStatus status, uint256 voteWeight);
-  event VotingExecuted(uint256 indexed votingId, VotingResult result);
+  event VotePlaced(address voter, VoteStatus status, uint256 voteWeight);
+
+  // event VotingExecuted(uint256 indexed votingId, VotingResult result);
 
   //Function
   receive() external payable {}
@@ -69,42 +70,43 @@ contract Governance {
   function getGovernanceName() public view returns (string memory) {
     return governanceName;
   }
+
   function getProposal() public view returns (string memory) {
     return proposal;
   }
 
-  function getStartTimestamp(uint votingId) public view returns (uint256) {
+  function getStartTimestamp(uint256 votingId) public view returns (uint256) {
     return votingDetails.createdVote;
   }
 
-  function getEndTimestamp(uint votingId) public view returns (uint256) {
+  function getEndTimestamp(uint256 votingId) public view returns (uint256) {
     return votingDetails.endVote;
   }
 
   function setWhitelist(address[] memory addresses) public onlyOwner {
     for (uint256 i = 0; i < addresses.length; i++) {
-      whitelistAddress[addresses[i]] = true;
+      whitelistAddress[addresses[i]].inWhitelist = true;
     }
   }
 
   function checkAddressWhitelist(address _address) external view returns (bool) {
-    return whitelistAddress[_address];
+    return whitelistAddress[_address].inWhitelist;
   }
 
-  function changeStartTimestamp(uint256 _createdVote) external onlyOwner {
+  function changeStartTimestamp(uint64 _createdVote) external onlyOwner {
     require(votingDetails.endVote < _createdVote, "Start timestamp must be less from the end");
     votingDetails.createdVote = _createdVote;
   }
 
-  function changeEndTimestamp(uint256 _endVote) external onlyOwner {
+  function changeEndTimestamp(uint64 _endVote) external onlyOwner {
     require(_endVote > votingDetails.createdVote, "End timestamp must be longer from the begin");
     votingDetails.endVote = _endVote;
   }
 
-  function vote(uint256 votingId, VoteStatus status) external isWhitelisted {
-    require(whitelistAddress[msg.sender].userWhitelist.executed == true, "The voter already voted");
+  function vote(VoteStatus status) external isWhitelisted {
+    require(whitelistAddress[msg.sender].executed == true, "The voter already voted");
     uint256 accountWeight = ERC20(governanceAddress).balanceOf(msg.sender);
-    votingDetails.voteByAccount[msg.sender] = status;
+    votings[msg.sender] = status;
 
     if (status == VoteStatus.For) {
       votingDetails.totalForVotes++;
@@ -112,36 +114,14 @@ contract Governance {
       votingDetails.totalAgainstVotes++;
     } else votingDetails.totalAbstainVotes++;
 
-    emit VotePlaced(votingId, msg.sender, status, accountWeight);
+    emit VotePlaced(msg.sender, status, accountWeight);
   }
 
-  function getVoteOf(uint256 votingId, address voter) external view returns (VoteStatus) {
-    VotingDetails storage votingDetails = votings[votingId];
-
-    return votingDetails.voteByAccount[voter];
+  function getVoteOf(address voter) external view returns (VoteStatus) {
+    return votings[voter];
   }
 
-  function getVoting(uint256 votingId)
-    external
-    view
-    returns (
-      uint256 createdAt,
-      uint256 duration,
-      uint256 totalForVotes,
-      uint256 totalAgainstVotes,
-      uint256 totalAbstainVotes,
-      bool executed
-    )
-  {
-    VotingDetails storage votingDetails = votings[votingId];
-
-    return (
-      votingDetails.createdAt,
-      votingDetails.duration,
-      votingDetails.totalForVotes,
-      votingDetails.totalAgainstVotes,
-      votingDetails.totalAbstainVotes,
-      votingDetails.executed
-    );
+  function getVoting() external view returns (VotingDetails memory) {
+    return votingDetails;
   }
 }
